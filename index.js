23580bdc9cc50d109f4389f7c28a725d316de2b2
Alex Carlson <alex@acwd.me>
2015-10-12 04:11:34 -0500

    updated character creator

--- index.js
+++ index.js
@@ -4,12 +4,20 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var bp = require('body-parser');
 
+var createdPlayers = [];
+
 app.use(bp.urlencoded({ extended:false })) //some garbage to enable parsing of the body
 app.use(bp.json()) // enable parsing of JSON files
 app.use(express.static('public')); //use express in static mode to look into the 'public' folder for files (.html, .jpg, .css, etc.)
 
 io.on('connection', function(socket){
   io.emit('players', io.sockets.sockets.length);
+
+  socket.on('submitPlayer', function (data) {
+    createdPlayers.push(data);
+    console.log("playerCount: "+createdPlayers.length);
+    io.emit('playerList', createdPlayers);
+  });
   
   socket.on('disconnect', function(){
     io.emit('players', io.sockets.sockets.length);
