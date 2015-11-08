854cc6d99577446194235c381c5a49ad76f202f8
Alex Carlson <alex@acwd.me>
2015-11-07 20:11:53 -0600

    converted to p5js

---
+++ public/sketch.js
@@ -0,0 +1,63 @@
+var myImage;
+var activeColor = 'black';
+var radius = 15;
+var myCanvas;
+
+function setup(){
+    myCanvas = createCanvas(350, 350);
+    myCanvas.parent('draw');
+    colorMode(HSB);
+    noStroke();
+}
+
+function mouseDragged(){
+    x = mouseX;
+    y = mouseY;
+    drawCircle(x, y, radius, activeColor);
+}
+
+function drawCircle( x, y, radius, color ) {
+    fill(color);
+    ellipse(x, y, radius, radius);
+} 
+
+$('.colors span').click(function(){
+    $('.colors span').removeClass('active');
+    activeColor = $(this).attr('data-color');
+    $(this).addClass('active');
+});
+
+$('#brushSize').change(function(){
+    radius = $(this).val();
+})
+
+function getImage(data, imgs){
+  for(j = 0; j < imgs.length; j++){
+
+    var thisIMG = $('.votingSection img:eq('+j+')');    
+    $('.votingSection img:eq('+j+')').removeClass('disabled');
+
+    var d = data[imgs[j]];
+
+    thisIMG.attr('src', d.drawing);
+    thisIMG.attr('data-player', d.user);
+
+    
+  }
+  $('section .votingSection').fadeIn(500);
+}
+
+// on submit drawing
+
+$('#submit').click(function(){
+    myCanvas.loadPixels();
+    var c = myCanvas.canvas;
+    var d = c.toDataURL();
+    var data = {user: uniqueID, drawing: d, score: 0};
+    socket.emit('sendPicture', data);
+    $('.myImage img').attr('src', data.drawing);
+
+    $('.draw').fadeOut(500);
+    $('.wait').fadeIn(500);
+    $('.myImage').fadeIn(500);
+})
